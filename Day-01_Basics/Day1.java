a225084a1d6efe0644a3199a09e3a301e8ad071c
iamflash0703 <nayaksovit18@gmail.com>
2026-02-14 09:01:45 +0530

    Add files via upload

---
+++ Day-01_Basics/Day1.java
@@ -0,0 +1,18 @@
+public class Day1{
+    public static void main(String[] args){
+
+        System.out.println("My name is Shovit");
+
+        for(int i =1;i<=5;i++){
+            System.out.println("Count : "+ i );
+        }
+
+        int age = 20;
+        if(age>=18){
+            System.out.println("Eligible for driving ");
+        }
+        else{
+            System.out.println("Not eligible for driving ");
+        }
+    }
+}
